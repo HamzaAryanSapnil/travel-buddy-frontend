2a23822ead5e082b1dc42a4efef5d42ed52f06b1
Hamza Aryan Sapnil <hamzaswapnil@gmail.com>
2025-12-12 03:19:57 +0600

    testing backend url in vercel

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -6,6 +6,7 @@ import Statistics from "@/components/modules/Home/Statistics";
 // export const dynamic = 'force-static'; // Removed to allow cookie reading in layout
 
 export default function Home() {
+  console.log("my backend url is: ", process.env.NEXT_PUBLIC_BASE_API_URL);
   return (
     <main>
       <Hero />
